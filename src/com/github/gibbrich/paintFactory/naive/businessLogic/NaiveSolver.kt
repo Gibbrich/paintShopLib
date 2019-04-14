e415bc8f08355e197f244867b0061038f0d6e115
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2019-04-14 16:28:35 +0300

    Added comment on naive solution

--- src/com/github/gibbrich/paintFactory/naive/businessLogic/NaiveSolver.kt
+++ src/com/github/gibbrich/paintFactory/naive/businessLogic/NaiveSolver.kt
@@ -9,7 +9,17 @@ import java.util.*
 
 @Deprecated("Use OptimizedSolver instead")
 object NaiveSolver {
-    fun solve(customers: List<NaiveCustomer>): Set<Solution> {
+    fun solveCases(cases: List<NaiveCase>): List<String> =
+            cases
+                    .map(::pickOptimalSolution)
+                    .mapIndexed(::getStringAnswer)
+
+    /**
+     * Iterates through all the customers; for each color from customer wishlist remember
+     * it as a possible solution. In such way combines solutions for next customer.
+     * This algorithm is ineffective, as it's complexity growth exponentially.
+     */
+    private fun solve(customers: List<NaiveCustomer>): Set<Solution> {
         if (customers.isEmpty()) {
             return emptySet()
         }
@@ -47,79 +57,7 @@ object NaiveSolver {
         return solutions
     }
 
-    fun pickOptimalSolutionParallel(case: NaiveCase): IntArray? {
-        return case.customers
-//        .parallelStream()
-            .map(NaiveSolver::toSolutionsSet)
-            .reduce(NaiveSolver::combineSolutions)
-            .minBy {
-                it.paints.values.sum()
-            }
-            ?.let {
-                getIntArray(it, case.paintsQty)
-            }
-    }
-
-    private fun toSolutionsSet(it: NaiveCustomer): Set<Solution> {
-        return it.paints
-            .map {
-                val map = TreeMap<Int, Int>().apply {
-                    put(it.key, it.value)
-                }
-                Solution(map)
-            }
-            .toSet()
-    }
-
-    fun combineSolutions(currentSolutions: Set<Solution>, acc: Set<Solution>): Set<Solution> {
-        if (acc.isEmpty() or currentSolutions.isEmpty()) {
-            return acc
-        }
-
-        val result = mutableSetOf<Solution>()
-        for (previousSolution in acc) {
-            for (currentSolution in currentSolutions) {
-                for (paint in currentSolution.paints) {
-                    val value = previousSolution.paints.get(paint.key)
-
-                    if (value != null) {
-                        if (value == paint.value) {
-                            result.add(previousSolution)
-                        }
-                    } else {
-                        val solution =
-                            Solution(previousSolution.paints.plus(paint.key to paint.value).toSortedMap())
-                        result.add(solution)
-                    }
-                }
-            }
-        }
-
-        println(result)
-
-        return result
-    }
-
-    fun pickOptimalSolutionParallel2(case: NaiveCase): IntArray? {
-        val arr = case.customers
-            .map(NaiveSolver::toSolutionsSet)
-            .toTypedArray()
-
-        asdf(arr)
-
-        return arr[arr.lastIndex].minBy {
-            it.paints.values.sum()
-        }
-            ?.let {
-                getIntArray(it, case.paintsQty)
-            }
-    }
-
-    fun asdf(array: Array<Set<Solution>>) {
-        Arrays.parallelPrefix(array, NaiveSolver::combineSolutions)
-    }
-
-    fun pickOptimalSolution(case: NaiveCase): IntArray? =
+    private fun pickOptimalSolution(case: NaiveCase): IntArray? =
         solve(case.customers)
             .minBy {
                 it.paints.values.sum()
@@ -127,11 +65,6 @@ object NaiveSolver {
             ?.let {
                 getIntArray(it, case.paintsQty)
             }
-
-    fun solveCases(cases: List<NaiveCase>): List<String> =
-        cases
-            .map(NaiveSolver::pickOptimalSolution)
-            .mapIndexed(NaiveSolver::getStringAnswer)
 
     private fun getStringAnswer(index: Int, solution: IntArray?): String {
         val answer = solution
